037272d25e4ca35be8405b75d336bc4c552fb866
gosaibk96 <gosaibk96@gmail.com>
2026-09-08 22:23:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ ST_PERIOD = 10
 ST_MULTIPLIER = 1.5
 
 COINS_TO_TRADE = [
-    {"symbol": "BTCUSD",   "product_id": 27,     "timeframe": "5m",  "lot_size": 2,   "target_pts": 500.0, "sl_pts": 100.0, "tsl_pts": 30.0},
-    {"symbol": "XAUTUSD",  "product_id": 131253, "timeframe": "5m",  "lot_size": 50, "target_pts": 25.0,  "sl_pts": 5.0,  "tsl_pts": 0.5},
-    {"symbol": "ETHUSD",   "product_id": 3136,   "timeframe": "5m",  "lot_size": 5,  "target_pts": 50.0,  "sl_pts": 10.0,  "tsl_pts": 1.0},
-    {"symbol": "SOLUSD",   "product_id": 120,    "timeframe": "15m",  "lot_size": 0,   "target_pts": 5.0,   "sl_pts": 3.0,   "tsl_pts": 1.5},
+    {"symbol": "BTCUSD",   "product_id": 27,     "timeframe": "5m",  "lot_size": 8,   "target_pts": 750.0, "sl_pts": 250.0, "tsl_pts": 30.0},
+    {"symbol": "XAUTUSD",  "product_id": 131253, "timeframe": "15m",  "lot_size": 100, "target_pts": 30.0,  "sl_pts": 10.0,  "tsl_pts": 2.0},
+    {"symbol": "ETHUSD",   "product_id": 3136,   "timeframe": "15m",  "lot_size": 10,  "target_pts": 30.0,  "sl_pts": 10.0,  "tsl_pts": 2.0},
+    {"symbol": "SOLUSD",   "product_id": 14823,    "timeframe": "15m",  "lot_size": 2,   "target_pts": 1.50,   "sl_pts": 0.50,   "tsl_pts": 0.10},
     {"symbol": "COINXUSD", "product_id": 125551, "timeframe": "15m",  "lot_size": 0,  "target_pts": 20.0,  "sl_pts": 12.0,  "tsl_pts": 6.0},
-    {"symbol": "LINKUSD",  "product_id": 15041,  "timeframe": "15m",  "lot_size": 0,  "target_pts": 0.50,  "sl_pts": 0.30,  "tsl_pts": 0.15},
-    {"symbol": "SLVONUSD", "product_id": 124058, "timeframe": "15m",  "lot_size": 0,  "target_pts": 0.40,  "sl_pts": 0.25,  "tsl_pts": 0.12},
+    {"symbol": "LINKUSD",  "product_id": 15041,  "timeframe": "15m",  "lot_size": 10,  "target_pts": 0.450,  "sl_pts": 0.150,  "tsl_pts": 0.10},
+    {"symbol": "SLVONUSD", "product_id": 124058, "timeframe": "15m",  "lot_size": 40,  "target_pts": 1.00,  "sl_pts": 0.25,  "tsl_pts": 0.05},
 ]
 
 CONTRACT_SIZE_MAP = {
